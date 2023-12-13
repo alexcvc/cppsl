9d20d56a52edae213aa3ade04735eb56979db6f1
A.Sacharov <a.sacharov@gmx.de>
2023-12-13 20:57:20 +0100

    Add detailed documentation to result.hpp classes
    Comment blocks were introduced to all the classes and methods in the 'result.hpp' file, making the code easier to understand. These classes and methods now have detailed explanations about their functionality, purpose and usage.

--- include/cppsl/result.hpp
+++ include/cppsl/result.hpp
@@ -30,7 +30,12 @@
 
 namespace cppsl {
 
-  // ////////////////////////////////////////////////////////////////////////////
+/**
+ * @file ResultOptVal.h
+ *
+ * @brief This file contains the declaration of the ResultOptVal class template.
+ */
+// ////////////////////////////////////////////////////////////////////////////
   /// result using std::optional
   /// It is incorrect to inherit from a standard library class.
   /// The result must be marked as final so that it cannot serve as a base class. 
@@ -43,24 +48,74 @@ namespace cppsl {
   template<typename T>
   class ResultOptVal {
    public:
+    /**
+     * @class ResultOptVal
+     * @brief A class that represents a result using std::optional. It provides methods for checking if the result is valid or invalid, retrieving the result value, and getting a default
+    * value if no result is present.
+     *
+     * It is important to note that this class does not inherit from a standard library class to avoid incorrect behavior. It is marked as final so that it cannot be used as a base class
+    *. Instead, it contains an std::optional member for storing the result value.
+     *
+     * @tparam T The type of the result value.
+     */
     constexpr ResultOptVal(T const &&t) noexcept
        : _opt_result_value{t} {
     }
 
+    /**
+     * @class ResultOptVal
+     * @brief A class that represents a result using std::optional.
+     *
+     * The ResultOptVal class provides methods for checking if the result is valid or invalid,
+     * retrieving the result value, and getting a default value if no result is present.
+     *
+     * @tparam T The type of the result value.
+     */
     explicit constexpr ResultOptVal() noexcept = default;
 
+    /**
+     * @brief Check if the result value is valid.
+     *
+     * This method determines if the result value is valid by checking if `_opt_result_value` has a value.
+     *
+     * @return true if the result value is valid, false otherwise.
+     */
     [[nodiscard]] constexpr bool valid() const noexcept {
       return _opt_result_value.has_value();
     }
 
+    /**
+     * @brief Check if the result value is invalid.
+     *
+     * This method determines if the result value is invalid by checking if `_opt_result_value` does not have a value.
+     *
+     * @return true if the result value is invalid, false otherwise.
+     */
     [[nodiscard]] constexpr bool invalid() const noexcept {
       return !valid();
     }
 
+    /**
+     * @class ResultOptVal
+     * @brief A class that represents a result using std::optional. It provides methods for checking if the result is valid or invalid, retrieving the result value, and getting a default
+    * value if no result is present.
+     *
+     * It is important to note that this class does not inherit from a standard library class to avoid incorrect behavior. It is marked as final so that it cannot be used as a base class
+    *. Instead, it contains an std::optional member for storing the result value.
+     *
+     * @tparam T The type of the result value.
+     */
     [[nodiscard]] constexpr auto get() const -> T {
       return _opt_result_value.value();
     }
 
+    /**
+     * @brief Retrieves the result value or a default value if no result is present.
+     *
+     * This method returns the result value stored in `_opt_result_value`. If there is no result value, it returns a default-constructed value of type `T`.
+     *
+     * @return The result value, or a default-constructed value of type `T` if no result is present.
+     */
     [[nodiscard]] auto get_or() const noexcept -> T {
       return _opt_result_value.value_or(T());
     }
@@ -76,20 +131,46 @@ namespace cppsl {
   // ////////////////////////////////////////////////////////////////////////////
   template<typename T>
   struct ResultVarVal : protected std::variant<std::monostate, T> {
+    /**
+     * @class ResultVarVal
+     * Represents a result value that can hold a variable or void.
+     */
     explicit constexpr ResultVarVal() noexcept = default;
 
+    /**
+     * @class ResultVarVal
+     * Represents a result value that can hold a variable or void.
+     */
     constexpr ResultVarVal(T const &&t) noexcept
        : std::variant<std::monostate, T>{t} {
     }
 
+    /**
+     * @brief Check if the ResultVarVal object holds a valid value.
+     *
+     * @return true if the ResultVarVal object holds a valid value, false otherwise.
+     */
     constexpr bool valid() const noexcept {
       return std::holds_alternative<T>(*this);
     }
 
+    /**
+     * @brief Check if the ResultVarVal object holds an invalid value.
+     *
+     * @return true if the ResultVarVal object holds an invalid value, false otherwise.
+     */
     constexpr bool invalid() const noexcept {
       return !valid();
     }
 
+    /**
+     * @brief Get the value stored in the ResultVarVal object.
+     *
+     * If the ResultVarVal object holds a valid value, returns that value.
+     * If the ResultVarVal object holds an invalid value, returns a default-constructed value of type T.
+     *
+     * @return The value stored in the ResultVarVal object.
+     */
     constexpr auto get() const noexcept -> T {
       return (valid() ? std::get<T>(*this) : T());
     }
@@ -108,19 +189,66 @@ namespace cppsl {
   struct ResultOptional : protected std::optional<T> {
     explicit constexpr ResultOptional() noexcept = default;
 
+    /**
+     * @class ResultOptional
+     *
+     * @brief Result based on std::optional
+     *
+     * The ResultOptional class is a templated class that is based on std::optional. It provides a result type that either contains a valid value of type T or is empty. This class is designed
+    * to be a wrapper around std::optional and adds some convenience methods for checking the validity of the result and accessing the value.
+     *
+     * Usage:
+     * ```
+     * ResultOptional<int> result(42);
+     * if (result.valid()) {
+     *     int value = result.get();
+     *     // Do something with value
+     * } else {
+     *     // Handle error case
+     * }
+     * ```
+     *
+     * @tparam T The type of the value contained in the result
+     */
     constexpr ResultOptional(T const &&t) noexcept
        : std::optional<T>{t} {
     }
 
+    /**
+     * @brief Check if the ResultOptional object is valid.
+     *
+     * This function checks if the ResultOptional object contains a valid value.
+     * It returns true if the object has a value, and false otherwise.
+     *
+     * @return True if the object has a value, false otherwise.
+     */
     [[nodiscard]] constexpr bool valid() const noexcept {
       //        return bool( *this);
       return std::optional<T>::has_value();
     }
 
+    /**
+     * @brief Check if the ResultOptional object is invalid.
+     *
+     * This function checks if the ResultOptional object is invalid. An object is considered
+     * invalid if it does not contain a valid value. It returns true if the object is invalid,
+     * and false otherwise.
+     *
+     * @return True if the object is invalid, false otherwise.
+     */
     [[nodiscard]] constexpr bool invalid() const noexcept {
       return !valid();
     }
 
+    /**
+     * @brief Get the value contained in the ResultOptional object.
+     *
+     * This member function returns the value contained in the ResultOptional object. If the object has a value, that value is returned. If the object is empty, a default-constructed value
+    * of type T is returned.
+     *
+     * @tparam T The type of the value.
+     * @return The value contained in the ResultOptional object.
+     */
     [[nodiscard]] constexpr auto get() const noexcept -> T {
       return std::optional<T>::value_or(T());
     }
